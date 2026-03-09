ffb6f54c04354da7198105b1dbfe526ab439e93a
Umair <umiiideveloper@gmail.com>
2026-03-09 11:39:25 +0500

    add the test case of model performance checking fixes minor bug of all  path in testing file

--- testing/testing_model.py
+++ testing/testing_model.py
@@ -7,10 +7,9 @@ from sklearn.metrics import accuracy_score
 class TestModelPipeline(unittest.TestCase):
 
     def test_1_load_model_and_transformer(self):
-        """Test whether model and transformer load successfully"""
 
         try:
-            with open("./models/model.pkl", "rb") as f:
+            with open("./models/rf_model.pkl", "rb") as f:
                 model = pickle.load(f)
 
             with open("./models/column_transformer.pkl", "rb") as f:
@@ -24,7 +23,6 @@ class TestModelPipeline(unittest.TestCase):
 
 
     def test_2_load_holdout_data(self):
-        """Test loading of holdout dataset"""
 
         try:
             data = pd.read_csv("data/interim/test.csv")
@@ -32,32 +30,26 @@ class TestModelPipeline(unittest.TestCase):
             self.fail(f"Loading holdout dataset failed: {e}")
 
         self.assertGreater(len(data), 0, "Holdout dataset is empty")
-        self.assertIn("target", data.columns, "Target column missing in dataset")
+        self.assertIn("Churn", data.columns, "Target column missing in dataset")
 
 
     def test_3_model_performance(self):
-        """Test if model accuracy is >= 70%"""
 
-        # Load model and transformer
-        with open("models/rf_model.pkl", "rb") as f:
+        with open("./models/rf_model.pkl", "rb") as f:
             model = pickle.load(f)
 
-        with open("models/column_transformer.pkl", "rb") as f:
+        with open("./models/column_transformer.pkl", "rb") as f:
             transformer = pickle.load(f)
 
-        # Load holdout data
         data = pd.read_csv("data/interim/test.csv")
 
         X = data.drop("Churn", axis=1)
         y = data["Churn"]
 
-        # Transform features
         X_transformed = transformer.transform(X)
 
-        # Predict
         predictions = model.predict(X_transformed)
 
-        # Evaluate
         accuracy = accuracy_score(y, predictions)
 
         print(f"\nModel Accuracy: {accuracy}")
